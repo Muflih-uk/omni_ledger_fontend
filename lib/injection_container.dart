7ee1046914827567d5ca0321eafa1f9ace342a3b
Muflih-uk <muhammedmuflih875@gmail.com>
2026-04-10 22:15:13 +0530

    feat: setup injection of fetch items

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -8,16 +8,21 @@ import 'core/storage/local_storage.dart';
 
 import 'features/auth/data/data_sources/auth_local_data_source.dart';
 import 'features/auth/data/data_sources/auth_remote_data_source.dart';
+import 'features/inventory/data/data_sources/item_remote_data_source.dart';
 
 // Repository
 import 'features/auth/data/repositories/auth_repository_impl.dart';
 import 'features/auth/domain/repositories/auth_repository.dart';
+import 'features/inventory/data/repositories/item_repository_impl.dart';
+import 'features/inventory/domain/repositories/item_repositories.dart';
 
 // Usecase
 import 'features/auth/domain/usecases/login_usecase.dart';
+import 'features/inventory/domain/usecases/items_usecases.dart';
 
 // Bloc
 import 'features/auth/presentation/bloc/auth_bloc.dart';
+import 'features/inventory/presentation/bloc/item_bloc.dart';
 
 final sl = GetIt.instance;
 
@@ -45,13 +50,16 @@ Future<void> init() async {
 
   //! ------------------ DATA SOURCES ------------------
 
-  // ✅ Use Dio from DioClient
   sl.registerLazySingleton<AuthRemoteDataSource>(
     () => AuthRemoteDataSourceImpl(sl<DioClient>().dio),
   );
 
   sl.registerLazySingleton<AuthLocalDataSource>(
     () => AuthLocalDataSourceImpl(sl()),
+  );
+
+  sl.registerLazySingleton<ItemRemoteDataSource>(
+    () => ItemRemoteDataSourceImpl(sl<DioClient>().dio),
   );
 
   //! ------------------ REPOSITORY ------------------
@@ -63,11 +71,17 @@ Future<void> init() async {
     ),
   );
 
+  sl.registerLazySingleton<ItemRepositories>(
+    () => ItemRepositoryImpl(sl<ItemRemoteDataSource>()),
+  );
+
   //! ------------------ USECASES ------------------
 
   sl.registerLazySingleton(() => LoginUsecase(sl()));
+  sl.registerLazySingleton(() => ItemsUsecases(sl()));
 
   //! ------------------ BLOC ------------------
 
   sl.registerFactory(() => AuthBloc(sl()));
+  sl.registerFactory(() => ItemBloc(sl()));
 }
